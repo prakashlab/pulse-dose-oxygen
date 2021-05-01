0e99916ef1a2c032316ef608fb55abb8fa803e7e
Ethan Li <lietk12@gmail.com>
2021-04-30 18:39:41 -0700

    Reenable pathPrefix but try to also set basePath

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,13 @@ module.exports = {
     title: 'Pulse-Dose Oxygen Conservation',
     description: 'Documentation site for a pulse-dose oxygen conservation system'
   },
+  pathPrefix: '/pulse-dose-oxygen',
   plugins: [
-    'gatsby-theme-documentation'
+    {
+      resolve: 'gatsby-theme-documentation',
+      options: {
+        basePath: '/pulse-dose-oxygen',
+      }
+    }
   ]
 }
